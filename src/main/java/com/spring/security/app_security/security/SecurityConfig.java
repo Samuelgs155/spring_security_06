864404f540d6fd133e9348950d24e4e809e61c58
Samuelgs155 <samuelgs15@hotmail.com>
2026-06-11 18:01:20 +0200

    roles_annontations

--- src/main/java/com/spring/security/app_security/security/SecurityConfig.java
+++ src/main/java/com/spring/security/app_security/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
@@ -26,6 +27,7 @@ import javax.sql.DataSource;
 import java.util.List;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
     //@Autowired
@@ -48,7 +50,7 @@ public class SecurityConfig {
                             .requestMatchers("/loans/**").hasAuthority("VIEW_LOANS")
                             .requestMatchers("/balance/**").hasAuthority("VIEW_BALANCE")
                             .requestMatchers("/cards/**").hasAuthority("VIEW_CARDS")
-                            .requestMatchers("/accounts/**").hasAnyAuthority("VIEW_ACCOUNT","VIEW_CARDS")
+                            // .requestMatchers("/accounts/**").hasAnyAuthority("VIEW_ACCOUNT","VIEW_CARDS")
                             // .authenticated()
                             // .requestMatchers("/welcome","/about_us").permitAll()
                             .anyRequest().permitAll()
